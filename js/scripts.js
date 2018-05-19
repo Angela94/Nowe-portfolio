8880a89fd1651832508953370f159e4c5060daec
Angela <siejek.angelika@gmail.com>
2018-05-19 22:13:53 +0200

    update

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+/* Smooth Scroll */
+
+$('a.smoth-scroll').on("click", function (e) {
+    var anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top - 50
+    }, 1000);
+    e.preventDefault();
+});
